79b653bd79009308d36082706fbf291e04df40e6
Darius Jonathan Lee <dariusjonathanlee@gmail.com>
2024-10-12 17:12:33 +0000

    add usb mgr to task

--- App/utils/src/task_mgr.c
+++ App/utils/src/task_mgr.c
@@ -7,6 +7,8 @@
 
 #include "utils.h"
 
+#include "usb_mgr.h"
+
 /* Defines -------------------------------------------------------------------*/
 
 /* Private Typedef -----------------------------------------------------------*/
@@ -16,30 +18,27 @@
 /* Private Function Declaration ----------------------------------------------*/
 
 /* Public Function Defnition -------------------------------------------------*/
-void taskMgr_initMcu( void )
+void taskMgr_initMcu(void)
 {
-    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-    HAL_Init();
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 
-    /* Configure the system clock */
-    SystemClock_Config();
+  /* Configure the system clock */
+  SystemClock_Config();
 
-    /* Initialize all configured peripherals */
-    MX_GPIO_Init();
-    MX_SPI1_Init();
-    MX_CRC_Init();
-
-    utils_printLog("Peripherals initialized");
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_SPI1_Init();
+  MX_CRC_Init();
 }
 
-void taskMgr_queueTask( void )
+void taskMgr_queueTask(void)
 {
-    utils_printLog("task queued");
+  usbMgr_queueTask();
 }
 
-void taskMgr_startTask( void )
+void taskMgr_startTask(void)
 {
-  utils_printLog("Starting task");
   osKernelInitialize();
   osKernelStart();
 }
