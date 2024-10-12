4acec049827136e5a22af5bac6f4230dd7b8c557
Darius Jonathan Lee <dariusjonathanlee@gmail.com>
2024-10-12 17:08:15 +0000

    Create usb manager

---
+++ App/usb/usb_mgr/inc/usb_mgr.h
@@ -0,0 +1,24 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __USB_MGR_H__
+#define __USB_MGR_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+/* Includes ------------------------------------------------------------------*/
+#include "usbd_cdc.h"
+
+/* Defines -------------------------------------------------------------------*/
+
+/* Public Typedef -----------------------------------------------------------*/
+
+/* Public Variable Declaration ----------------------------------------------*/
+
+/* Public Function Declaration ----------------------------------------------*/
+void usbMgr_queueTask( void );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __USB_MGR_H__ */
